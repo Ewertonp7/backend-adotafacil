36d8c9d8314eaa42ce9f2f246364fce2e0666966
Ewertonp7 <elspereira@sga.pucminas.br>
2025-05-05 21:34:34 -0300

    Atualizando backend com novos arquivos e ajustes

--- server.js
+++ server.js
@@ -2,35 +2,32 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const recuperarSenhaRoutes = require("./src/routes/recuperarSenhaRoutes");
+const animaisRoutes = require('./src/routes/animais');
 
 const app = express();
 
-
-
-// Middlewares
 app.use(express.json());
 app.use(cors());
 
-// rec senha
 app.use("/api/recuperar-senha", recuperarSenhaRoutes);
 
-// Conexão com o banco de dados
 require('./src/config/db');
 
-// Importar rotas
 const uploadRoutes = require('./src/routes/upload.routes');
 const authRoutes = require('./src/routes/authRoutes');
 const usuariosRoutes = require('./src/routes/usuarios');
 
-// Usar rotas
-app.use('/api', uploadRoutes);       // Rota para upload: /api/upload
-app.use('/api/auth', authRoutes);    // Rotas de autenticação: /api/auth/login, etc.
-app.use('/api/usuarios', usuariosRoutes); // /api/usuarios/:id
+app.use('/api', uploadRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/usuarios', usuariosRoutes);
+
+// Montar as rotas de animais sob o prefixo /api/animais
+app.use('/api/animais', animaisRoutes); // CORRIGIDO AQUI
+
 
 console.log('Rotas de upload foram carregadas!');
 console.log('Rotas de usuários carregadas!');
 
-// Rota de teste
 app.get('/', (req, res) => {
     res.send('API funcionando');
 });
@@ -39,4 +36,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
-
